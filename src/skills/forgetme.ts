1bf34b8c2c5c49a4bbb68853e7ee6e53ca79a983
Fabian Schäfer <fschaefer0@gmail.com>
2017-10-01 12:55:01 +0200

    Fix "forget me" skill

--- src/skills/forgetme.ts
+++ src/skills/forgetme.ts
@@ -4,9 +4,10 @@ export function handleForgetMe(bot: SlackBot, message: Message, users: Storage<U
 
     users.get(message.user || "", (err: Error, user: User) => {
 
-        if (user && users.delete) {
-
-            users.delete(message.user as string, (error: Error) => {
+        if (user) {
+            // RedisStorage does not implement .delete but .remove
+            // see open PR https://github.com/howdyai/botkit-storage-redis/pull/13
+            (users as any).remove(message.user as string, (error: Error) => {
                 bot.reply(message, "Got it. I forgot about you.");
             });
         }
