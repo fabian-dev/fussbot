209ab4563587807fc96eb194af819d14a2c35196
Fabian Schäfer <fschaefer0@gmail.com>
2017-10-01 12:00:05 +0200

    Move last example skill away from app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { SlackBot, SlackMessage } from "botkit";
 import { handleHello } from "./skills/hello";
 import { handleUptime } from "./skills/uptime";
 import { handleCallMe } from "./skills/callme";
+import { handleWhatIsMyName } from "./skills/whatismyname";
 
 const Botkit = require("botkit");
 const RedisStorage = require("botkit-storage-redis");
@@ -36,6 +37,9 @@ registerDirect(["hello", "hi"],
 registerDirect(["call me (.*)", "my name is (.*)"],
     (bot, message) => handleCallMe(bot, message, controller.storage.users));
 
+registerDirect(["what is my name", "who am i"],
+    (bot, message) => handleWhatIsMyName(bot, message, controller.storage.users));
+
 registerDirect(["uptime"], handleUptime);
 
 function registerDirect(keywords: string[],
@@ -44,72 +48,3 @@ function registerDirect(keywords: string[],
 
     controller.hears(keywords, directEvents, handler);
 }
-
-/*
-controller.hears(["what is my name", "who am i"], "direct_message,direct_mention,mention", function (bot, message) {
-
-    controller.storage.users.get(message.user, function (err, user) {
-        if (user && user.name) {
-            bot.reply(message, "Your name is " + user.name);
-        } else {
-            bot.startConversation(message, function (err, convo) {
-                if (!err) {
-                    convo.say("I do not know your name yet!");
-                    convo.ask("What should I call you?", function (response, convo) {
-                        convo.ask("You want me to call you `" + response.text + "`?", [
-                            {
-                                pattern: "yes",
-                                callback(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                },
-                            },
-                            {
-                                pattern: "no",
-                                callback(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                },
-                            },
-                            {
-                                default: true,
-                                callback(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                },
-                            },
-                        ]);
-
-                        convo.next();
-
-                    }, {key: "nickname"}); // store the results in a field called nickname
-
-                    convo.on("end", function (convo) {
-                        if (convo.status === "completed") {
-                            bot.reply(message, "OK! I will update my dossier...");
-
-                            controller.storage.users.get(message.user, function (err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse("nickname");
-                                controller.storage.users.save(user, function (err, id) {
-                                    bot.reply(message, "Got it. I will call you " + user.name + " from now on.");
-                                });
-                            });
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, "OK, nevermind!");
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
-*/
---
+++ src/skills/whatismyname.ts
@@ -0,0 +1,72 @@
+import { Conversation, Message, SlackBot, SlackMessage, Storage, User } from "botkit";
+
+export function handleWhatIsMyName(bot: SlackBot, message: Message, users: Storage<User>) {
+
+    users.get(message.user || "", (err: Error, user: User) => {
+
+        if (user && user.name) {
+            bot.reply(message, "Your name is " + user.name);
+
+        } else {
+            bot.startConversation(message, (err: Error, convo: Conversation<SlackMessage>) => {
+                if (!err) {
+                    convo.say("I do not know your name yet!");
+
+                    convo.ask("What should I call you?", (answer: SlackMessage, convo: Conversation<SlackMessage>) => {
+
+                        convo.ask("You want me to call you `" + answer.text + "`?", [
+                            {
+                                pattern: "yes",
+                                callback(answer: SlackMessage, convo: Conversation<SlackMessage>) {
+                                    // since no further messages are queued after this,
+                                    // the conversation will end naturally with status == 'completed'
+                                    convo.next();
+                                },
+                            },
+                            {
+                                pattern: "no",
+                                callback(answer: SlackMessage, convo: Conversation<SlackMessage>) {
+                                    // stop the conversation. this will cause it to end with status == 'stopped'
+                                    convo.stop();
+                                },
+                            },
+                            {
+                                default: true,
+                                callback(answer: SlackMessage, convo: Conversation<SlackMessage>) {
+                                    convo.repeat();
+                                    convo.next();
+                                },
+                            },
+                        ]);
+
+                        convo.next();
+
+                    }, { key: "nickname" }); // store the results in a field called nickname
+
+                    convo.on("end", (convo: Conversation<SlackMessage>) => {
+                        if (convo.status === "completed") {
+
+                            bot.reply(message, "OK! I will update my dossier...");
+
+                            users.get(message.user || "", (err: Error, user: User) => {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                    } as User;
+                                }
+                                user.name = convo.extractResponse("nickname");
+                                users.save(user, (error: Error) => {
+                                    bot.reply(message, "Got it. I will call you " + user.name + " from now on.");
+                                });
+                            });
+
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, "OK, nevermind!");
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
